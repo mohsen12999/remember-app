2d28f1bc46f39f95dd4eec5c3bc61f5da89360f8
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-10-24 15:22:46 +0330

    make new project with electron-forge

--- renderer.js
+++
@@ -1,8 +0,0 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
-
-console.log("hello from renderer");
-
-
-const fileFunctions = require('./file-functions.js')
-const fileData = fileFunctions.readFileFromDisk()
