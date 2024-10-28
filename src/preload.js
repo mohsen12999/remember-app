a9ad10828b18e596db804250eee739a847c81f37
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-10-29 00:06:46 +0330

    update preload file for send and received data

--- src/preload.js
+++ src/preload.js
@@ -1,19 +1,19 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
-// const fs = require('fs')
+const { contextBridge, ipcRenderer } = require("electron");
 
-// const { contextBridge } = require('electron')
+// const project_config = require("./package.json");
 
 // contextBridge.exposeInMainWorld('versions', {
-//   node: () => process.versions.node,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron
+//   // node: () => process.versions.node,
+//   // chrome: () => process.versions.chrome,
+//   // electron: () => process.versions.electron
+//   app_version: () => project_config.version
 //   // we can also expose variables, not just functions
 // })
 
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveData: (data) => ipcRenderer.send('save-data', data),
-})
+contextBridge.exposeInMainWorld("electronAPI", {
+  saveData: (data) => ipcRenderer.send("save-data", data),
+  loadData: (callback) => ipcRenderer.on("load-data", callback),
+});
