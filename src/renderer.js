ef85fbf8192ab941de6edca3ed3fc0496a40f437
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-10-29 00:07:29 +0330

    show tasks in index.html

--- src/renderer.js
+++ src/renderer.js
@@ -1,36 +1,24 @@
-//     const fs = require('fs')
+const save_btn = document.getElementById("save_btn");
 
+save_btn.addEventListener("click", () => {
+  // find add checkboxes title
+  const allTasks = ["task1", "task2", "task3"];
+  // find all checked checkbox and retrieved their title
+  const doneTasks = ["task1", "task3"];
 
+  window.electronAPI.saveData([allTasks, doneTasks]);
+});
 
-// readFileFromDisk = () => {
-//     fs.readFile(FILE_NAME, 'utf-8', (err, data) => {
-//         if (err) {
-//             console.error(err)
-//             return DEFAULT_DATA
-//         }
-//         console.log(data)
-//     })
-// }
+window.electronAPI.loadData((event, data) => {
+  console.log("Received data from main:", data); // Handle received data
+  const all_tasks = data.tasks;
+  const tasks_ul = document.getElementById("task_list");
+  all_tasks.forEach((a_task) => {
+    const li_element = document.createElement("li");
+    li_element.innerHTML = `<label><input type="checkbox" /> ${a_task}</label>`; //a_task
+    tasks_ul.append(li_element);
+  });
 
-saveFileToDisk = (data) => {
-
-}
-
-// readFileFromDisk()
-
-const save_btn = document.getElementById('save_btn')
-
-save_btn.addEventListener('click',()=>{
-
-    // find add checkboxes title
-    const allTasks = ["task1", "task2" , "task3"]
-    // find all checked checkbox and retrieved their title
-    const doneTasks = ["task1" , "task3"]
-
-    window.electronAPI.saveData([allTasks, doneTasks])
-})
-
-
-// const information = document.getElementById('info')
-// information.innerText = "text"
-// information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+  const today_span = document.getElementById("today");
+  today_span.innerText = data.currentDate;
+});
