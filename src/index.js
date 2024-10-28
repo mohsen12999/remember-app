e15697e7a8b61da8602b16bfe220765adc63f9a4
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-10-28 21:52:46 +0330

    add load file function in index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-const { app, BrowserWindow, ipcMain  } = require('electron');
-const path = require('node:path');
-const fs = require('fs')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
+const fs = require("fs");
 
-const FILE_NAME = 'data.json'
+const FILE_NAME = "data.json";
 const DEFAULT_DATA = {
-    tasks: ["task1", "task2", "task3", "task4"],
-    data:[]
-}
+  tasks: ["task1", "task2", "task3", "task4"],
+  data: {},
+};
+
+const loadData = () => {
+  const currentDate = new Date().toISOString().split("T")[0]; // "2024-12-10"
+  const default_data = { ...DEFAULT_DATA, currentDate };
+
+  fs.readFile(FILE_NAME, "utf-8", (err, file_data) => {
+    if (err) {
+      console.log("error in reading file", err);
+      return default_data;
+    }
+    try {
+      const data = JSON.parse(file_data);
+      return { ...data, currentDate };
+    } catch (error) {
+      console.log("error in parsing file's data", error);
+      return default_data;
+    }
+  });
+
+  return data;
+};
 
 const handleSaveData = (event, tasks) => {
   console.log("save data in index js", tasks);
-  
+
   // fs.writeFile(FILE_NAME, data, (err) => {
   //     if (err) {
   //         console.error(err)
   //         return
   //     }
   // })
-}
+};
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -32,29 +53,34 @@ const createWindow = () => {
     webPreferences: {
       // contextIsolation: true,
       // nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
+  // remove menu bar
+  mainWindow.menuBarVisible = false;
+
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   // if (process.env.NODE_ENV != 'development')
   mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.send("load-data", loadData());
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  ipcMain.on('save-data', handleSaveData)
+  ipcMain.on("save-data", handleSaveData);
 
   createWindow();
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -64,8 +90,8 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
